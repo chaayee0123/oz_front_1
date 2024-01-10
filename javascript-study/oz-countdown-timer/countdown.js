ae449cda5fe74439bd51c208dc32485c93b07edc
chaayee0123 <chayee0614@gmail.com>
2024-01-10 17:52:23 +0900

    카운트다운

--- javascript-study/oz-countdown-timer/countdown.js
+++ javascript-study/oz-countdown-timer/countdown.js
@@ -1,21 +1,44 @@
-let countdown;
-countdown = setInterval(function(){
-    const days = document.getElementById("day")
-    const hours = document.getElementById("hour")
-    const minutes = document.getElementById("minute")
-    const seconds = document.getElementById("second")
+const countdown = document.querySelector(".countdown");
 
-    const lastday = new Date("12,31,2024,23:59:59").getTime();
-    const today = Date.now();
-    const diffDay = lastday - today;
-    const diffday = Math.floor(diffDay / (1000*3600*24));
-    const diffhour = Math.floor((diffDay % (1000*60*60*24))/ (1000 * 3600) );
-    const diffMin = Math.floor((diffDay % (1000*60*60)) / (1000*60));
-    const diffSec = Math.floor(diffDay % (1000 * 60) / 1000) ;
+    const interval = setInterval(function(){
+        const lastday = new Date("12,31,2024,23:59:59").getTime();
+        const today = Date.now();
+        const diffDay = lastday - today;
+        const diffday = Math.floor(diffDay / (1000*3600*24));
+        const diffhour = Math.floor((diffDay % (1000*60*60*24))/ (1000 * 3600) );
+        const diffMin = Math.floor((diffDay % (1000*60*60)) / (1000*60));
+        const diffSec = Math.floor(diffDay % (1000 * 60) / 1000) ;
 
-    days.textContent = diffday
-    hours.textContent = diffhour
-    minutes.textContent = diffMin
-    seconds.textContent = diffSec
+        countdown.innerHTML = `
+        <div data-content="Days">${diffday}</div>
+        <div data-content="Hours">${diffhour}</div>
+        <div data-content="Minutes">${diffMin}</div>
+        <div data-content="Seconds">${diffSec}</div>
+        `
+    },1000)
 
-},1000)
+
+
+
+
+// let countdown;
+// countdown = setInterval(function(){
+//     const days = document.getElementById("day")
+//     const hours = document.getElementById("hour")
+//     const minutes = document.getElementById("minute")
+//     const seconds = document.getElementById("second")
+
+//     const lastday = new Date("12,31,2024,23:59:59").getTime();
+//     const today = Date.now();
+//     const diffDay = lastday - today;
+//     const diffday = Math.floor(diffDay / (1000*3600*24));
+//     const diffhour = Math.floor((diffDay % (1000*60*60*24))/ (1000 * 3600) );
+//     const diffMin = Math.floor((diffDay % (1000*60*60)) / (1000*60));
+//     const diffSec = Math.floor(diffDay % (1000 * 60) / 1000) ;
+
+//     days.textContent = diffday
+//     hours.textContent = diffhour
+//     minutes.textContent = diffMin
+//     seconds.textContent = diffSec
+
+// },1000)
